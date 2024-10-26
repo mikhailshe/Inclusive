515a8229c99011e968bc8480916f2b4948260979
микемка <mikemka@vk.com>
2024-10-26 17:08:41 +0500

    initial

---
+++ inclusive_ai/main/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/homepage.html')
